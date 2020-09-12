bb71a21384fecef9dd8819b6487e501f40adbb74
siren92 <mmhamilton19@hotmail.com>
2020-09-11 22:37:49 -0400

    Changed the behavior of the bottom to change the text original greeting message. I did this by calling the button textView and setting it to the new message.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -2,7 +2,11 @@ package edu.temple.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+             TextView text_msg=findViewById(R.id.textView);
+             text_msg.setText("You have clicked the button");
+
+
+            }
+        });
     }
 }
+
+
+
